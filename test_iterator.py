9a3a24c1ed8ecd51d75ee311dd799da80a7148df
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2020-09-03 16:22:11 +0200

    plot changes to test file

--- test_iterator.py
+++ test_iterator.py
@@ -11,6 +11,7 @@ parspace = dyn.parameter_space.ParameterSpace(c.system)
 all_models = dyn.schwarzschild.AllModels(from_file=False,
                                          parspace=parspace,
                                          settings=c.settings)
+
 smi = dyn.shwarzschild_model_iterator.SchwarzschildModelIterator(
     system=c.system,
     all_models=all_models,
@@ -22,15 +23,27 @@ all_models.table['chi2']
 chi2 = all_models.table['chi2']+all_models.table['kinchi2']
 min_chi2 = np.min(chi2)
 del_chi2 = chi2 - min_chi2
+
+# plot
+kwscat = dict(cmap=plt.cm.viridis_r,
+              s=100)
 plt.scatter(all_models.table['mass'],
             all_models.table['ml'],
-            c=del_chi2)
+            c=del_chi2,
+            **kwscat)
 kwline = dict(color='k', lw=0.5)
-plt.axhline(parspace[0].grid_parspace_settings['lo'], **kwline)
-plt.axhline(parspace[0].grid_parspace_settings['hi'], **kwline)
-plt.axvline(parspace[7].grid_parspace_settings['lo'], **kwline)
-plt.axvline(parspace[7].grid_parspace_settings['hi'], **kwline)
+plt.axhline(parspace[0].grid_parspace_settings['lo'],
+            **kwline)
+plt.axhline(parspace[0].grid_parspace_settings['hi'],
+            **kwline)
+plt.axvline(parspace[7].grid_parspace_settings['lo'],
+            **kwline)
+plt.axvline(parspace[7].grid_parspace_settings['hi'],
+            **kwline)
+plt.colorbar()
 plt.show()
+
+
 
 
 
