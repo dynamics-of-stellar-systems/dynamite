a82d7fef9a3f77335b6df67df9ba7051dc716825
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2020-09-06 09:49:37 +0200

    save all_tables at the end of the test_iterator.py script

--- test_iterator.py
+++ test_iterator.py
@@ -4,75 +4,35 @@
 import numpy as np
 import dynamite_src as dyn
 import matplotlib.pyplot as plt
+import os
+from astropy.io import ascii
 
-'''
-model_example/NGC6278/
-    config_example.yaml
-    dynamite_script.py
-    input_data/
-
-pwd
->model_example/NGC6278/
-python dynamite_script.py
-
-model_example/NGC6278/
-    config_example.yaml
-    dynamite_script.py
-    input_data/
-    output_data/
-'''
-
-fname = 'model_example/config_legacy_example.yaml'
+# read configuration
+fname = 'pj_model/config_legacy_example.yaml'
 c = dyn.config_reader.ConfigurationReaderYaml(fname, silent=True)
 parspace = dyn.parameter_space.ParameterSpace(c.system)
-all_models = dyn.schwarzschild.AllModels(from_file=False,
-                                         parspace=parspace,
-                                         settings=c.settings)
 
+# read all existing models, or make empty all_models object if none
+fname = c.settings.io_settings['output_directory']+'all_models.ecsv'
+if os.path.isfile(fname):
+    all_models = dyn.schwarzschild.AllModels(from_file=True,
+                                             filename=fname,
+                                             parspace=parspace,
+                                             settings=c.settings)
+else:
+    all_models = dyn.schwarzschild.AllModels(from_file=False,
+                                             parspace=parspace,
+                                             settings=c.settings)
+
+# run models
 smi = dyn.shwarzschild_model_iterator.SchwarzschildModelIterator(
     system=c.system,
     all_models=all_models,
     settings=c.settings)
 
-all_models.table['kinchi2']
-all_models.table['chi2']
-
-chi2 = all_models.table['chi2']+all_models.table['kinchi2']
-min_chi2 = np.min(chi2)
-del_chi2 = chi2 - min_chi2
-
-
-np.where(np.array(parspace.par_names)=='f')
-
-parspace.par_names[2]
-parspace.par_names[7]
-
-all_models.table['dc']
-all_models.table['ml']
-
-
-# plot
-kwscat = dict(cmap=plt.cm.viridis_r,
-              s=100)
-plt.scatter(all_models.table['dc'],
-            all_models.table['ml'],
-            c=del_chi2,
-            **kwscat)
-kwline = dict(color='k', lw=0.5)
-plt.axvline(parspace[2].grid_parspace_settings['lo'],
-            **kwline)
-plt.axvline(parspace[2].grid_parspace_settings['hi'],
-            **kwline)
-plt.axhline(parspace[7].grid_parspace_settings['lo'],
-            **kwline)
-plt.axhline(parspace[7].grid_parspace_settings['hi'],
-            **kwline)
-plt.colorbar()
-plt.show()
-
-
-
-
+# save the all_models table
+fname = c.settings.io_settings['output_directory']+'all_models.ecsv'
+all_models.table.write(fname, format='ascii.ecsv')
 
 
 
