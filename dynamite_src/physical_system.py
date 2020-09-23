d6812c087b2746532312c50ab6fc116e4f145301
maindlt <thomas.maindl@univie.ac.at>
2020-09-23 10:58:18 +0300

    Config cleanup (#4)
    * Removed directory from all input files in the config file.
    Added input_directory to the Data init class
    infil/-entries: eliminated directory in config file and hardcoded "infil/" in schwarzschild.py
    
    * Now, Data reads the datafile only once...
    
    * Moved sformat from config file to component classes and system class.
    Converted desc to comment in config file.
    
    * Renamed "GridSearch" to "GridWalk"
    
    Co-authored-by: Thomas I. Maindl <thomas.maindl@sdb.ltd>

--- dynamite_src/physical_system.py
+++ dynamite_src/physical_system.py
@@ -47,10 +47,29 @@ class System(object):
 #        self.n_par += len(cmp.parameters)
 
     def validate(self):
-        if not(self.ml and self.distMPc and self.name and self.position_angle):
-            raise ValueError('System needs ml parameter and distMPc, name, and position_angle attributes')
+        """
+        Ensures the System has the required attributes, at least one component,
+        and the ml parameter.
+        Additionally, the sformat string for the ml parameter is set.
+
+        Raises
+        ------
+        ValueError : if required attributes or components are missing, or if
+                     there is no ml parameter
+
+        Returns
+        -------
+        None.
+
+        """
+        if not(self.distMPc and self.name and self.position_angle):
+            raise ValueError('System needs distMPc, name, '
+                             'and position_angle attributes')
         if not self.cmp_list:
             raise ValueError('System has no components')
+        if len(self.parameters) != 1 and self.parameters[0].name != 'ml':
+            raise ValueError('System needs ml as its sole parameter')
+        self.parameters[0].update(sformat = '6.2f')
 
     def __repr__(self):
         return f'{self.__class__.__name__} with {self.__dict__}'
@@ -59,13 +78,13 @@ class System(object):
 class Component(object):
 
     def __init__(self,
-                 name = None,                     # string
-                 visible=None,                    # Boolean
-                 contributes_to_potential=None,   # Boolean
-                 symmetry=None,                   # OPTIONAL, spherical, axisymm, or triax
-                 kinematic_data=[],               # a list of Kinematic objects
-                 population_data=[],              # a list of Population objects
-                 parameters=[]):                  # a list of Parameter objects
+                 name = None,                    # string
+                 visible=None,                   # Boolean
+                 contributes_to_potential=None,  # Boolean
+                 symmetry=None,       # OPTIONAL, spherical, axisymm, or triax
+                 kinematic_data=[],              # a list of Kinematic objects
+                 population_data=[],             # a list of Population objects
+                 parameters=[]):                 # a list of Parameter objects
         if name == None:
             self.name = self.__class__.__name__
         else:
@@ -79,9 +98,29 @@ class Component(object):
         self.parameters = parameters
         # self.validate()
 
-    def validate(self, par=None):
+    def validate(self, par_format=None):
+        """
+        Ensures the Component has the required attributes and parameters.
+        Additionally, the sformat strings for the parameters are set.
+
+        Parameters
+        ----------
+        par_format : a dict with parameter_name:sformat pairs. Mandatory.
+
+        Raises
+        ------
+        ValueError : if a required attribute is missing or the required
+                     parameters do not exist
+
+        Returns
+        -------
+        None.
+
+        """
         # if self.symmetry not in self.symmetries:
-        #     raise ValueError('Illegal symmetry ' + str(self.symmetry) + '. Allowed: ' + str(self.symmetries))
+        #     raise ValueError('Illegal symmetry ' + str(self.symmetry) + \
+        #                      '. Allowed: ' + str(self.symmetries))
+        par = par_format.keys()
         errstr = f'Component {self.__class__.__name__} needs attribute '
         if self.visible is None:
             raise ValueError(errstr + 'visible')
@@ -91,10 +130,18 @@ class Component(object):
             raise ValueError(errstr + 'parameters')
 
         # if len(self.parameters) != len(par):
-        #     raise ValueError(f'{self.__class__.__name__} needs exactly {len(par)} paramater(s), not {len(self.parameters)}')
+        #     raise ValueError(f'{self.__class__.__name__} needs exactly '
+        #         f'{len(par)} paramater(s), not {len(self.parameters)}')
         if set([p.name for p in self.parameters]) != set(par):
-            raise ValueError(f'{self.__class__.__name__} needs parameters {par}, not {[p.name for p in self.parameters]}')
+            raise ValueError(f'{self.__class__.__name__} needs parameters '
+                             f'{par}, not {[p.name for p in self.parameters]}')
+        self.set_format(par_format)
 
+    def set_format(self, par_format=None):
+        if par_format is None:
+            raise ValueError(f'{self.__class__.__name__}: no format string')
+        for p in self.parameters:
+            p.update(sformat = par_format[p.name])
 
     def __repr__(self):
         return (f'\n{self.__class__.__name__}({self.__dict__}\n)')
@@ -112,7 +159,8 @@ class VisibleComponent(Component):
     def validate(self, **kwds):
         super().validate(**kwds)
         if not isinstance(self.mge, mge.MGE):
-            raise ValueError(f'{self.__class__.__name__}.mge must be mges.MGE object')
+            raise ValueError(f'{self.__class__.__name__}.mge must be '
+                             'mges.MGE object')
 
 
 class AxisymmetricVisibleComponent(VisibleComponent):
@@ -121,8 +169,8 @@ class AxisymmetricVisibleComponent(VisibleComponent):
         super().__init__(symmetry='axisymm', **kwds)
 
     def validate(self):
-        par = ['par1', 'par2']
-        super().validate(par=par)
+        par_format = {'par1':'6.2f', 'par2':'6.3f'}
+        super().validate(par_format)
 
 
 class TriaxialVisibleComponent(VisibleComponent):
@@ -131,8 +179,8 @@ class TriaxialVisibleComponent(VisibleComponent):
         super().__init__(symmetry='triax', **kwds)
 
     def validate(self):
-        par = ['q', 'p', 'u']
-        super().validate(par=par)
+        par_format = {'q':'6.2f', 'p':'6.3f', 'u':'7.4f'}
+        super().validate(par_format=par_format)
 
     def triax_pqu2tpp(self,p,q,qobs,u):
         """
@@ -211,10 +259,11 @@ class Plummer(DarkComponent):
         return rho
 
     def validate(self):
-        par = ['mass', 'a']
-        super().validate(par=par)
+        par_format = {'mass':'6.2f', 'a':'7.2f'}
+        super().validate(par_format)
         # if len(self.parameters) != 2:
-        #     raise ValueError(f'{self.__class__.__name__} needs exactly 2 paramaters, not {len(self.parameters)}')
+        #     raise ValueError(f'{self.__class__.__name__} needs exactly 2 '
+        #                      f'paramaters, not {len(self.parameters)}')
 
 
 class NFW(DarkComponent):
@@ -224,10 +273,11 @@ class NFW(DarkComponent):
         super().__init__(symmetry='spherical', **kwds)
 
     def validate(self):
-        par = ['dc', 'f']
-        super().validate(par=par)
+        par_format = {'dc':'6.2f', 'f':'6.2f'}
+        super().validate(par_format)
         # if len(self.parameters) != 2:
-        #     raise ValueError(f'{self.__class__.__name__} needs exactly 2 paramaters, not {len(self.parameters)}')
+        #     raise ValueError(f'{self.__class__.__name__} needs exactly 2 '
+        #                      f'paramaters, not {len(self.parameters)}')
 
 
 class Hernquist(DarkComponent):
@@ -237,10 +287,11 @@ class Hernquist(DarkComponent):
         super().__init__(symmetry='spherical', **kwds)
 
     def validate(self):
-        par = ['rhoc', 'rc']
-        super().validate(par=par)
+        par_format = {'rhoc':'6.2f', 'rc':'6.2f'}
+        super().validate(par_format)
         # if len(self.parameters) != 2:
-        #     raise ValueError(f'{self.__class__.__name__} needs exactly 2 paramaters, not {len(self.parameters)}')
+        #     raise ValueError(f'{self.__class__.__name__} needs exactly 2 '
+        #                      f'paramaters, not {len(self.parameters)}')
 
 
 class TriaxialCoredLogPotential(DarkComponent):
@@ -250,10 +301,11 @@ class TriaxialCoredLogPotential(DarkComponent):
         super().__init__(symmetry='triaxial', **kwds)
 
     def validate(self):
-        par = ['Vc', 'rho', 'p', 'q']
-        super().validate(par=par)
+        par_format = {'Vc':'6.2f', 'rho':'6.2f', 'p':'6.2f', 'q':'6.2f'}
+        super().validate(par_format)
         # if len(self.parameters) != 4:
-        #     raise ValueError(f'{self.__class__.__name__} needs exactly 4 paramaters, not {len(self.parameters)}')
+        #     raise ValueError(f'{self.__class__.__name__} needs exactly 4 '
+        #                      f'paramaters, not {len(self.parameters)}')
 
 
 class GeneralisedNFW(DarkComponent):
@@ -263,10 +315,12 @@ class GeneralisedNFW(DarkComponent):
         super().__init__(symmetry='triaxial', **kwds)
 
     def validate(self):
-        par = ['concentration', 'Mvir', 'inner_log_slope']
-        super().validate(par=par)
+        par_format = {'concentration':'6.2f', 'Mvir':'6.2f',
+                      'inner_log_slope':'6.2f'}
+        super().validate(par_format)
         # if len(self.parameters) != 3:
-        #     raise ValueError(f'{self.__class__.__name__} needs exactly 3 paramaters, not {len(self.parameters)}')
+        #     raise ValueError(f'{self.__class__.__name__} needs exactly 3 '
+        #                      f'paramaters, not {len(self.parameters)}')
 
 
 
