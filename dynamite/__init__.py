5c17ff72fa37a61424cf751d542a5f4e65bc1bda
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2020-10-11 15:52:01 +0200

    Fiddle with version defintion in order to be able to print dynamite.__version__
    Use this solution: 
    https://stackoverflow.com/a/17626524/11231128

--- dynamite/__init__.py
+++ dynamite/__init__.py
@@ -1,1 +1,2 @@
 from dynamite import data, orblib, physical_system, weight_solvers, parameter_space, model, config_reader, model_iterator, executor, kinematics, plotter
+from dynamite._version import __version__
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
-from importlib.machinery import SourceFileLoader
 
-# this loads the version number from the acmesuperservice/version.py module
-version = SourceFileLoader("dynamite.version", "dynamite/version.py").load_module()
+# this loads the version number from the dynamite/version.py module
+version = open("dynamite/_version.py")
+version = version.readlines()[-1].split()[-1].strip("\"'")
 
 # load the readme as long description
 with open("README.md", "r") as fh:
@@ -14,13 +14,13 @@ with open("requirements.txt", "r") as fp:
 
 setuptools.setup(
     name="dynamite",
-    version=version.__version__,
-    author="Sabine Thater, Prashin Jethwa, Thomas Maindl",
+    version=version,
+    author="Prashin Jethwa, Sabine Thater, Thomas Maindl",
     author_email="prashin.jethwa@univie.ac.at",
     description="dynamics, age and metallicity indicators tracing evolution",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.hub.musimap.io/our_repo",
+    url="https://www.univie.ac.at/dynamics/dynamite_docs/index.html",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
