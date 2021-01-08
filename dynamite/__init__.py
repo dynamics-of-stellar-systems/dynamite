7c3cafff0e895cab7905d3ee7997b1db35c57472
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2021-01-08 22:28:27 +0100

    Included myrand.py in dynamite Python setup, added some output to myrantest.py.

--- dynamite/__init__.py
+++ dynamite/__init__.py
@@ -1,2 +1,2 @@
-from dynamite import data, orblib, physical_system, weight_solvers, parameter_space, model, config_reader, model_iterator, executor, kinematics, plotter
+from dynamite import data, orblib, physical_system, weight_solvers, parameter_space, model, config_reader, model_iterator, executor, kinematics, plotter, myrand
 from dynamite._version import __version__
--- tests/myrantest.py
+++ tests/myrantest.py
@@ -3,13 +3,18 @@ import myrand
 
 seed = -4242
 
-print(f"Python random numbers for seed = {seed}:")
+print("Comparison of portable random number generator myrand.py and"
+      f" ran1_nr.f for seed={seed}")
+
+print("Python random numbers from myrand.py:")
 mr = myrand.MyRand(seed)
 for i in range(10):
     print(mr.ran1())
 
-print(f"Fortran random numbers for seed = {seed}:")
+print("Fortran random numbers from ran1_nr.f:")
 subprocess.call(f"sed -i -e 's/^idum.*/idum = {seed}/' rantest.f90", shell=True)
 compilestring = "gfortran -o rantest rantest.f90 ../legacy_fortran/ran1_nr.f"
 runstring = "./rantest"
 subprocess.call(f"{compilestring}\n{runstring}", shell=True)
+print("Fortran version:")
+subprocess.call("gfortran --version", shell=True)
