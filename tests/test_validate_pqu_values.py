f82b2f0810af0927f372062b2a17f964d3cd94db
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2021-02-09 23:47:38 +0100

    test_validate_pqu_values.py now creates its own output folder if necessary...

--- tests/test_validate_pqu_values.py
+++ tests/test_validate_pqu_values.py
@@ -28,6 +28,10 @@ print('Located at:', dyn.__path__)
 # read configuration
 fname = 'test_validate_pqu_config.yaml'
 c = dyn.config_reader.Configuration(fname, reset_logging=True)
+io_settings = c.settings.io_settings
+outdir = io_settings['output_directory']
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
 
 # stars = c.system.get_component_from_name('stars')
 #
