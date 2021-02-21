0a56addd8b1c83022b3776d53d594fe5c742755d
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2021-02-21 14:31:42 +0100

    (1) Moved the test script to the tests folder (2) It now plots kinematic maps for all kinematics sets and saves them

--- tests/run_dyn_single.py
+++ tests/run_dyn_single.py
@@ -7,6 +7,8 @@ Created on Mon Feb 15 08:54:48 2021
 """
 
 import dynamite as dyn
+import os
+import shutil
 
 
 print('DYNAMITE')
@@ -15,7 +17,19 @@ print('    installed at ', dyn.__path__)
 
 fname = 'FCC047_2kin/FCC047_config.yaml'
 
-c = dyn.config_reader.Configuration(fname)
+c = dyn.config_reader.Configuration(fname, reset_logging=True)
+
+io_settings = c.settings.io_settings
+outdir = io_settings['output_directory']
+# delete previous output if available
+models_folder = outdir + 'models/'
+models_file = outdir + io_settings['all_models_file']
+shutil.rmtree(models_folder, ignore_errors=True)
+if os.path.isfile(models_file):
+    os.remove(models_file)
+plotdir = outdir + 'plots/'
+if not os.path.isdir(plotdir):
+    os.mkdir(plotdir)
 
 print(type(c.system))
 print(type(c.settings))
@@ -23,8 +37,8 @@ print(type(c.settings))
 parset = c.parspace.get_parset()
 print(parset)
 
-
-
+nkin = c.system.n_kin
+print(f'{nkin} kinematics data sets in system')
 
 model = dyn.model.LegacySchwarzschildModel(
     system=c.system,
@@ -49,7 +63,10 @@ plotter = dyn.plotter.Plotter(system=c.system,
                               parspace=c.parspace,
                               all_models=c.all_models)
 
-figure = plotter.plot_kinematic_maps(model,kin_set=1)
-
-
-
+for kinset in range(nkin):
+    plotfile = f'{plotdir}kin_map{kinset}.png'
+    if os.path.isfile(plotfile):
+        os.remove(plotfile)
+    figure = plotter.plot_kinematic_maps(model,kin_set=kinset+1)
+    figure.savefig(plotfile)
+    print(f'Look at {plotfile}')
