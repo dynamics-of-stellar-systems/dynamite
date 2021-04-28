7c5184426ef53c9e2b772713a0c930b0921652bb
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2021-04-28 11:52:48 +0300

    Fixed a bug that prevented successful execution of test scripts via `python script.py` (rather than executing via `./script.py`).

--- dev_tests/test_nnls.py
+++ dev_tests/test_nnls.py
@@ -29,7 +29,9 @@ def run_user_test(make_comp=False):
 
     # read configuration
     if '__file__' in globals():
-        os.chdir(os.path.dirname(__file__))
+        file_dir = os.path.dirname(__file__)
+        if file_dir:
+            os.chdir(file_dir)
     fname = 'user_test_config_ml.yaml'
     c = dyn.config_reader.Configuration(fname, reset_logging=True)
 
