2a414fffc8cd286b272ccf4a1ded98f612ea351a
Thomas Maindl <thomas.maindl@sdb.ltd>
2021-06-30 22:28:16 +0300

    Promote logging after legacy Fortran returns w/o error from debug -> info and include Fortran stderr+stdout. Reason: legacy Fortran exits with STOP (rather than ERROR STOP) when it encounters inconsistencies; this does not set an exit code != 0 and may happen undetected causing DYNAMITE errors down the road. Now this is detected easier.

--- dynamite/orblib.py
+++ dynamite/orblib.py
@@ -306,13 +306,13 @@ class LegacyOrbitLibrary(OrbitLibrary):
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
                            shell=True)
-        log_file = f'Logfile: {self.mod_dir}datfil/orbstart.log'
+        log_file = f'Message: {p.stdout.decode("UTF-8")}.' \
+                   f'Logfile: {self.mod_dir}datfil/orbstart.log.'
         if p.returncode == 0:
-            self.logger.debug(f'...done - {cmdstr} exit code {p.returncode}. '
+            self.logger.info(f'...done - {cmdstr} exit code {p.returncode}. '
                               f'{log_file}')
         else:
-            text = f'{cmdstr} exit code {p.returncode}. ' \
-                   f'Message: {p.stdout}. {log_file}'
+            text = f'{cmdstr} exit code {p.returncode}. ERROR. {log_file}'
             self.logger.error(text)
             raise RuntimeError(text)
         os.chdir(cur_dir)
@@ -343,15 +343,15 @@ class LegacyOrbitLibrary(OrbitLibrary):
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
                            shell=True)
-        log_files = f'Logfiles: {self.mod_dir}datfil/orblib.log, ' \
+        log_files = f'Message: {p.stdout.decode("UTF-8")}.' \
+                    f'Logfiles: {self.mod_dir}datfil/orblib.log, ' \
                     f'{self.mod_dir}datfil/triaxmass.log, ' \
-                    f'{self.mod_dir}datfil/triaxmassbin.log'
+                    f'{self.mod_dir}datfil/triaxmassbin.log.'
         if p.returncode == 0:
-            self.logger.debug(f'...done - {cmdstr_tube} exit code '
+            self.logger.info(f'...done - {cmdstr_tube} exit code '
                               f'{p.returncode}. {log_files}')
         else:
-            text = f'{cmdstr_tube} exit code {p.returncode}. ' \
-                   f'Message: {p.stdout}. {log_files}'
+            text=f'{cmdstr_tube} exit code {p.returncode}. ERROR. {log_files}'
             self.logger.error(text)
             raise RuntimeError(text)
         self.logger.info('Integrating orbit library box orbits')
@@ -360,13 +360,13 @@ class LegacyOrbitLibrary(OrbitLibrary):
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
                            shell=True)
-        log_file = f'Logfile: {self.mod_dir}datfil/orblibbox.log'
+        log_file = f'Message: {p.stdout.decode("UTF-8")}.' \
+                   f'Logfile: {self.mod_dir}datfil/orblibbox.log.'
         if p.returncode == 0:
-            self.logger.debug(f'...done - {cmdstr_box} exit code '
+            self.logger.info(f'...done - {cmdstr_box} exit code '
                               f'{p.returncode}. {log_file}')
         else:
-            text = f'{cmdstr_box} exit code {p.returncode}. ' \
-                   f'Message: {p.stdout}. {log_file}'
+            text = f'{cmdstr_box} exit code {p.returncode}. ERROR. {log_file}'
             self.logger.error(text)
             raise RuntimeError(text)
         # move back to original directory
