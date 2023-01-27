c95eb737ae6587dc3fdef39323880046d0a5b97a
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2023-01-27 15:35:32 +0100

    test_decomp.py now generates an all_models table instead of relying on an existing one.

--- dev_tests/test_decomp.py
+++ dev_tests/test_decomp.py
@@ -9,9 +9,11 @@ fname = 'user_test_config_ml.yaml'
 c = dyn.config_reader.Configuration(fname,
                                     reset_logging=True,
                                     user_logfile='test_decomp',
-                                    reset_existing_output=False)
+                                    reset_existing_output=True)
 
-decomp = dyn.orbit_exploration.Decomposition(c)
+dyn.model_iterator.ModelIterator(c) # generate models
+
+decomp = dyn.orbit_exploration.Decomposition(c) # do the decomposition
 
 for conversion in ('gh_expand_around_losvd_mean_and_std_deviation',
                    'gh_fit_with_free_v_sigma_params_fortran',
