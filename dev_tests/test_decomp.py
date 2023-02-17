fc63552ab4f13648c7d8bdab0dcec8ad40230a52
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2023-02-17 11:11:52 +0200

    Added the kinematics set to test_decomp.py.

--- dev_tests/test_decomp.py
+++ dev_tests/test_decomp.py
@@ -6,6 +6,7 @@
 import dynamite as dyn
 
 fname = 'user_test_config_ml.yaml'
+#fname = 'FCC047_2kin/FCC047_config.yaml'
 c = dyn.config_reader.Configuration(fname,
                                     reset_logging=True,
                                     user_logfile='test_decomp',
@@ -13,7 +14,8 @@ c = dyn.config_reader.Configuration(fname,
 
 dyn.model_iterator.ModelIterator(c) # generate models
 
-decomp = dyn.orbit_exploration.Decomposition(c) # do the decomposition
+decomp = dyn.orbit_exploration.Decomposition(config=c,
+                                             kin_set=0) # do the decomposition
 
 for conversion in ('gh_expand_around_losvd_mean_and_std_deviation',
                    'gh_fit_with_free_v_sigma_params_fortran',
