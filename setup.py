bedd97bf4be222877e15ec0b94e49cee3cb26294
Thomas I. Maindl <thomas.maindl@univie.ac.at>
2023-08-11 17:58:19 +0300

    Updated requirements (#317)
    * Updated requirements.
    Aligned required python version >= 3.8 with documentation.
    
    * Fix legacy weight solver installation issues.
    
    ---------
    
    Co-authored-by: Thomas I. Maindl <thomas.maindl@sdb.ltd>

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ legacy_fortran = [
     "../legacy_fortran/triaxmass",
     "../legacy_fortran/triaxmassbin",
 ]
-additional_ex = ["../legacy_fortran/modelgen",
-                 "../legacy_fortran/triaxnnls_CRcut",
-                 "../legacy_fortran/triaxnnls_noCRcut"]
-legacy_fortran.extend([e for e in additional_ex if os.path.isfile(e)])
+additional_ex = ["legacy_fortran/modelgen",
+                 "legacy_fortran/triaxnnls_CRcut",
+                 "legacy_fortran/triaxnnls_noCRcut"]
+legacy_fortran.extend([f'../{e}' for e in additional_ex if os.path.isfile(e)])
 
 setuptools.setup(
     name="dynamite",
@@ -38,13 +38,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     project_urls={
         "Source": "https://github.com/dynamics-of-stellar-systems/dynamite/",
         "Documentation": "https://dynamics.univie.ac.at/dynamite_docs/index.html",
     },
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     # use the already parsed requirements from requirements.txt
     install_requires=required,
     package_data={
