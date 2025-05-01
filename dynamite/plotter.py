cc6230663c23643f3dde711801d76ae76d6c0b3a
yu <126140360+yu-2410@users.noreply.github.com>
2025-05-01 16:56:06 +0800

    fix: normalize chi2 value for colormap to avoid exceeding 1.0 (#451)

--- dynamite/plotter.py
+++ dynamite/plotter.py
@@ -256,7 +256,8 @@ class Plotter():
                 for k in range(nf - 1, -1, -1):
                     if val['chi2t'][k]/chlim<=3: #only significant chi2 values
 
-                        color = colormap(val['chi2t'][k]/chlim)
+                        norm = mpl.colors.Normalize(vmin=0., vmax=3)
+                        color = colormap(norm(val['chi2t'][k]/chlim))
                         # * 240) #colours the significant chi2
 
                         markersize = 15-3*(val['chi2t'][k]/(chlim))
