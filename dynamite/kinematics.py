09c2fac6cce0964f0794f4542c31c2b17dcd7d87
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2025-09-17 22:27:18 +0300

    In case kinematics errors are zero: rather than letting the Python weight solver crash, issue an error message pointing to the faulty data.

--- dynamite/kinematics.py
+++ dynamite/kinematics.py
@@ -294,6 +294,22 @@ class GaussHermite(Kinematics, data.Integrated):
                               'applied systematic errors.')
             if cache_data:
                 self._data_with_sys_err = gh_data.copy(copy_data=True)
+        zero_err = [(int(bin) + 1, 'dv')
+                    for bin in np.nonzero(gh_data['dv'] == 0)
+                    if len(bin) > 0]
+        zero_err += [(int(bin) + 1, 'dsigma')
+                    for bin in np.nonzero(gh_data['dsigma'] == 0)
+                    if len(bin) > 0]
+        for i in range(3, number_gh + 1):
+            zero_err += [(int(bin) + 1, f'dh{i}')
+                         for bin in np.nonzero(gh_data[f'dh{i}'] == 0)
+                         if len(bin) > 0]
+        if len(zero_err) > 0:
+            txt = 'Kinematics uncertainties cannot be zero, consider editing '
+            txt += 'the kinematics datafile(s) and/or GH_sys_err. Violating '
+            txt += f'vbin_id / data_id pair(s): {zero_err}.'
+            self.logger.error(txt)
+            raise ValueError(txt)
         return gh_data
 
     def has_pops(self):
@@ -1004,6 +1020,12 @@ class BayesLOSVD(Kinematics, data.Integrated):
             self.data.remove_column(f'losvd_{j}')
             losvd_sigma[:,j] = self.data[f'dlosvd_{j}']
             self.data.remove_column(f'dlosvd_{j}')
+        zero_err = np.nonzero(losvd_sigma == 0)
+        if len(zero_err[0]) > 0:
+            txt = 'Kinematics uncertainties cannot be zero. Violating '
+            txt += f'binID / vbin pair(s): {list(zip(*zero_err))}.'
+            self.logger.error(txt)
+            raise ValueError(txt)
         self.data['losvd'] = losvd_mean
         self.data['dlosvd'] = losvd_sigma
 
