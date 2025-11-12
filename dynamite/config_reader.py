6badd128f1c1d91bcf1d43bffed6b360d5dc0af6
Thomas I. Maindl <thomas.maindl@univie.ac.at>
2025-11-12 17:50:23 +0200

    Bugfix for bar and histograms in general (#475)
    * When the number of velocity histogram bins is even, exit with a sensible error message upon reading the config file rather than after orblib integration.
    
    * Bugfix: the 'disk_lum' property of the BarDisk should actually be filled from the file linked to 'disk_lum', not 'disk_pot'.
    Fixed the bartest.py by changing the velocity histogram number of bins to an odd number in bartest.yaml.

--- dynamite/config_reader.py
+++ dynamite/config_reader.py
@@ -356,7 +356,7 @@ class Configuration(object):
                     if 'disk_lum' in data_comp:
                         path = self.settings.io_settings['input_directory']
                         c.disk_lum = mge.MGE(input_directory=path,
-                                             datafile=data_comp['disk_pot'])
+                                             datafile=data_comp['disk_lum'])
 
                     # add component to system
                     c.validate()
@@ -1027,6 +1027,19 @@ class Configuration(object):
                     max_bins += 1
                 for k in stars.kinematic_data:
                     k.hist_bins = max_bins
+        else:  # enforce odd number of histogram bins
+            if self.system.is_bar_disk_system():
+                stars = self.system.get_unique_bar_component()
+            else:
+                stars = self.system.get_unique_triaxial_visible_component()
+            hist_bins = [k.hist_bins % 2 for k in stars.kinematic_data]
+            if any([h == 0 for h in hist_bins]):
+                all_hist_bins = {k.name: k.hist_bins
+                                 for k in stars.kinematic_data}
+                txt = 'Value of hist_bins must be odd for all kinematic ' \
+                      f'data, but they are {all_hist_bins}.'
+                self.logger.error(txt)
+                raise ValueError(txt)
 
     def validate_chi2(self, which_chi2=None):
         """
