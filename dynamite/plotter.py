bugfix: insert missing factors of u in MGE mass calculation

dynamite/plotter.py
@@ -1023,14 +1023,15 @@ class Plotter():
         phi = incl[1]
         psi = incl[2]
 
-        pintr, qintr = self.triax_tpp2pqu(theta=theta, phi=phi, psi=psi,
-                                          qobs=qobs_pot, psi_off=psi_off,
-                                          res=1)[:2]
+        pintr, qintr, uintr = self.triax_tpp2pqu(theta=theta, phi=phi, psi=psi,
+                                                qobs=qobs_pot, psi_off=psi_off,
+                                                res=1)
         p_pot = np.copy(pintr)
         q_pot = np.copy(qintr)
         sig_pot_pc = np.copy(sigobs_pot_pc)
-        dens_pot_pc = surf_pot_pc*qobs_pot/(np.sqrt(2.*np.pi)*
-                        sig_pot_pc*q_pot*p_pot)
+        sig_intr_pc = sig_pot_pc / uintr
+        dens_pot_pc = surf_pot_pc * (2 * np.pi * sig_pot_pc**2*qobs_pot)/\
+                ((sig_intr_pc*np.sqrt(2*np.pi))**3*q_pot*p_pot)
 
         nr = len(r_pc)
         res=np.zeros(nr)