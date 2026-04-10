9a4c4172b1df4e0517175be9e4ee0fe65791fac6
Thomas I. Maindl <thomas.maindl@sdb.ltd>
2026-04-10 22:52:49 +0200

    Use implicit type casting.

--- dynamite/mges.py
+++ dynamite/mges.py
@@ -132,37 +132,37 @@ class MGE(data.Data):
             y2 = y*y
             z2 = z*z
 
-            A12 = -(A1[n] - A2[n])/(1. - pintr[n]**2)
+            A12 = -(A1[n] - A2[n])/(1 - pintr[n]**2)
             A23 = -(A2[n] - A3[n])/(pintr[n]**2 - qintr[n]**2)
-            A31 = -(A3[n] - A1[n])/(qintr[n]**2 - 1.)
+            A31 = -(A3[n] - A1[n])/(qintr[n]**2 - 1)
 
-            A11 = (1./3.)*(2./(1.) - A12 - A31)
-            A22 = (1./3.)*(2./(pintr[n]**2) - A23 - A12)
-            A33 = (1./3.)*(2./(qintr[n]**2) - A31 - A23)
+            A11 = 1/3 * (2 - A12 - A31)
+            A22 = 1/3 * (2/(pintr[n]**2) - A23 - A12)
+            A33 = 1/3 * (2/(qintr[n]**2) - A31 - A23)
 
-            O1 = -1./(2.*sigintr_km[n]**2)*(A1[n]*x2 + A2[n]*y2 + A3[n]*z2)
-            O2 = 1./(8.*sigintr_km[n]**4)* \
+            O1 = -1/(2*sigintr_km[n]**2)*(A1[n]*x2 + A2[n]*y2 + A3[n]*z2)
+            O2 = 1/(8*sigintr_km[n]**4)* \
                 (A11*x2*x2 + A22*y2*y2 + A33*z2*z2 +
-                2.*A12*x2*y2 + 2.*A23*y2*z2 + 2.*A31*z2*x2)
+                2*A12*x2*y2 + 2*A23*y2*z2 + 2*A31*z2*x2)
 
-            return V0[n]/math.sqrt(1. - qintr[n]**2)*(F[n] + O1 + O2)
+            return V0[n]/math.sqrt(1 - qintr[n]**2)*(F[n] + O1 + O2)
 
         def potmid(n, x, y, z):
 
             def potfunc(t, gx, gy, gz, gn):
-                d = 1. - ((1. - pintr[gn]*pintr[gn])*t*t)
-                e = 1. - ((1. - qintr[gn]*qintr[gn])*t*t)
+                d = 1 - ((1 - pintr[gn]*pintr[gn])*t*t)
+                e = 1 - ((1 - qintr[gn]*qintr[gn])*t*t)
                 # Integral part of formula 12 of Cappellari 2002.
-                a = math.exp(-t*t/(2.*sigintr_km[gn]**2)*
+                a = math.exp(-t*t/(2*sigintr_km[gn]**2)*
                             (gx*gx + (gy*gy)/d + (gz*gz)/e))
                 return a/math.sqrt(d*e)
 
-            epsabs = 1.49e-08  # 0.
-            epsrel = 1.49e-08  # 1.0e-8
-            limit = 50  # 20
+            epsabs = 1.49e-08
+            epsrel = 1.49e-08
+            limit = 50 # 20
             res = integrate.quad(potfunc,
-                                 0.,
-                                 1.,
+                                 0,
+                                 1,
                                  args=(x, y, z, n),
                                  epsabs=epsabs,
                                  epsrel=epsrel,
@@ -180,55 +180,55 @@ class MGE(data.Data):
             y2 = y*y
             z2 = z*z
 
-            A12 = -(A1[n] - A2[n])/(1. - pintr[n]**2)
+            A12 = -(A1[n] - A2[n])/(1 - pintr[n]**2)
             A23 = -(A2[n] - A3[n])/(pintr[n]**2 - qintr[n]**2)
-            A31 = -(A3[n] - A1[n])/(qintr[n]**2 - 1.)
+            A31 = -(A3[n] - A1[n])/(qintr[n]**2 - 1)
 
-            A11 = (1./3.)*(2./(1.) - A12 - A31)
-            A22 = (1./3.)*(2./(pintr[n]**2) - A23 - A12)
-            A33 = (1./3.)*(2./(qintr[n]**2) - A31 - A23)
+            A11 = 1/3 * (2 - A12 - A31)
+            A22 = 1/3 * (2/(pintr[n]**2) - A23 - A12)
+            A33 = 1/3 * (2/(qintr[n]**2) - A31 - A23)
 
-            vx = -V0[n]/math.sqrt(1.0 - qintr[n]**2)*x/sigintr_km[n]**2* \
-                (A1[n] - 1./(2.*sigintr_km[n]**2)*(A11*x2 + A12*y2 + A31*z2))
-            vy = -V0[n]/math.sqrt(1.0 - qintr[n]**2)*y/sigintr_km[n]**2* \
-                (A2[n] - 1./(2.*sigintr_km[n]**2)*(A12*x2 + A22*y2 + A23*z2))
-            vz = -V0[n]/math.sqrt(1.0 - qintr[n]**2)*z/sigintr_km[n]**2* \
-                (A3[n] - 1./(2.*sigintr_km[n]**2)*(A31*x2 + A23*y2 + A33*z2))
+            vx = -V0[n]/math.sqrt(1 - qintr[n]**2)*x/sigintr_km[n]**2* \
+                (A1[n] - 1/(2*sigintr_km[n]**2)*(A11*x2 + A12*y2 + A31*z2))
+            vy = -V0[n]/math.sqrt(1 - qintr[n]**2)*y/sigintr_km[n]**2* \
+                (A2[n] - 1/(2*sigintr_km[n]**2)*(A12*x2 + A22*y2 + A23*z2))
+            vz = -V0[n]/math.sqrt(1 - qintr[n]**2)*z/sigintr_km[n]**2* \
+                (A3[n] - 1/(2*sigintr_km[n]**2)*(A31*x2 + A23*y2 + A33*z2))
 
             return vx, vy, vz
 
         def accmid(n, x, y, z):
 
             def axfunc(t, gx, gy, gz, gn):
-                d = 1. - ((1. - pintr[gn]*pintr[gn])*t*t)
-                e = 1. - ((1. - qintr[gn]*qintr[gn])*t*t)
+                d = 1 - (1 - pintr[gn]*pintr[gn])*t*t
+                e = 1 - (1 - qintr[gn]*qintr[gn])*t*t
                 # Integral part of formula 12 of Cappellari 2002.
-                a = math.exp(-t*t/(2.*sigintr_km[gn]**2)*(gx*gx + (gy*gy)/d + (gz*gz)/e))
+                a = math.exp(-t*t/(2*sigintr_km[gn]**2)*(gx*gx + (gy*gy)/d + (gz*gz)/e))
                 # dF/dx
                 return -gx/sigintr_km[gn]**2*t*t*a/math.sqrt(d*e)
 
             def ayfunc(t, gx, gy, gz, gn):
-                d = 1. - ((1. - pintr[gn]*pintr[gn])*t*t)
-                e = 1. - ((1. - qintr[gn]*qintr[gn])*t*t)
+                d = 1 - (1 - pintr[gn]*pintr[gn])*t*t
+                e = 1 - (1 - qintr[gn]*qintr[gn])*t*t
 
                 # Integral part of formula 12 of Cappellari 2002.
-                a = math.exp(-t*t/(2.*sigintr_km[gn]**2)*(gx*gx + (gy*gy)/d + (gz*gz)/e))
+                a = math.exp(-t*t/(2*sigintr_km[gn]**2)*(gx*gx + (gy*gy)/d + (gz*gz)/e))
                 # dF/dy
                 return -gy/sigintr_km[gn]**2*t*t/d*a/math.sqrt(d*e)
 
             def azfunc(t, gx, gy, gz, gn):
                 # integral part of formula 12 of Cappellari 2002.
-                d = 1. - ((1. - pintr[gn]*pintr[gn])*t*t)
-                e = 1. - ((1. - qintr[gn]*qintr[gn])*t*t)
+                d = 1 - (1 - pintr[gn]*pintr[gn])*t*t
+                e = 1 - (1 - qintr[gn]*qintr[gn])*t*t
 
                 # Integral part of formula 12 of Cappellari 2002.
-                a = math.exp(-t*t/(2.*sigintr_km[gn]**2)*(gx*gx + (gy*gy)/d + (gz*gz)/e))
+                a = math.exp(-t*t/(2*sigintr_km[gn]**2)*(gx*gx + (gy*gy)/d + (gz*gz)/e))
                 # dF/dz
                 return -gz/sigintr_km[gn]**2*t*t/e*a/math.sqrt(d*e)
 
-            epsabs = 1.49e-08  # 0.
-            epsrel = 1.49e-08  # 1.0e-8
-            limit = 50  # 20
+            epsabs = 1.49e-08
+            epsrel = 1.49e-08
+            limit = 50
             res = integrate.quad(axfunc,
                                  0.,
                                  1.,
@@ -291,9 +291,9 @@ class MGE(data.Data):
 
         total_mass = math.tau * sum(surf_km * qobs * sigobs_km ** 2)
 
-        secth = 1. / math.cos(theta_view)
-        cotph = 1. / math.tan(phi_view)
-        delp = 1. - qobs ** 2
+        secth = 1 / math.cos(theta_view)
+        cotph = 1 / math.tan(phi_view)
+        delp = 1 - qobs ** 2
         nom1minq2 = \
             delp * (2 * np.cos(2 * psi_obs) +
                     np.sin(2 * psi_obs) * (secth * cotph -
@@ -305,12 +305,12 @@ class MGE(data.Data):
         denom = \
             2 * math.sin(theta_view)**2 * (delp * np.cos(psi_obs) *
                                            (np.cos(psi_obs) +
-                                            secth * cotph * np.sin(psi_obs)) - 1.)
+                                            secth * cotph * np.sin(psi_obs)) - 1)
         # These are temporary values of the squared intrinsic axial
         # ratios p^2 and q^2
-        qintr = (1. - nom1minq2 / denom)
+        qintr = (1 - nom1minq2 / denom)
         pintr = (qintr + nomp2minq2 / denom)
-        if any(qintr < 0.) or any(pintr < 0.):
+        if any(qintr < 0) or any(pintr < 0):
             txt = "p^2 or q^2 is below 0."
             self.logger.error(txt)
             raise ValueError(txt)
@@ -320,8 +320,8 @@ class MGE(data.Data):
         self.logger.debug(f'Middle axis ratio p={pintr}, '
                           f'minor axis ratio q={qintr}.')
         # triaxiality parameter T = (1-p^2)/(1-q^2)
-        triaxpar = (1. - pintr**2) / (1. - qintr**2)
-        if any(triaxpar < 0.) or any(triaxpar > 1.):
+        triaxpar = (1 - pintr**2) / (1 - qintr**2)
+        if any(triaxpar < 0) or any(triaxpar > 1):
             txt = 'No triaxial deprojection possible!'
             self.logger.error(txt)
             raise ValueError(txt)
@@ -330,7 +330,7 @@ class MGE(data.Data):
             txt = 'q > p'
             self.logger.error(txt)
             raise ValueError(txt)
-        if any(pintr > 1.):
+        if any(pintr > 1):
             txt = 'p > 1'
             self.logger.error(txt)
             raise ValueError(txt)
@@ -346,7 +346,7 @@ class MGE(data.Data):
         dens = surf_km * qobs * sigobs_km ** 2 / \
                 (math.sqrt(math.tau) * pintr * qintr * sigintr_km ** 3)
         # Integration Constant
-        V0 = 4. * math.pi * constants.GRAV_CONST_KM * \
+        V0 = 4 * math.pi * constants.GRAV_CONST_KM * \
               sigintr_km ** 2 * pintr * qintr * dens
         # Compute the constants for the inner approximation
         k = np.sqrt((1 - pintr ** 2) / (1 - qintr ** 2))
@@ -368,28 +368,28 @@ class MGE(data.Data):
 
         self.logger.info('Testing the accuracy of the approximation regimes.')
         inner_approx = 0.0001
-        outer_approx = 300.
+        outer_approx = 300
         for i in range(len(self.data)):
-            ax = potin(i, inner_approx * sigintr_km[i], 1., 1.)
-            ix = potmid(i, inner_approx * sigintr_km[i], 1., 1.)
+            ax = potin(i, inner_approx * sigintr_km[i], 1, 1)
+            ix = potmid(i, inner_approx * sigintr_km[i], 1, 1)
             if abs((ix - ax) / ix) > 1.0e-4:
                  txt = f"Failed test 1: {ix} != {ax}"
                  self.logger.error(txt)
                  raise ValueError(txt)
-            ax = potin(i, 1., 1., inner_approx * sigintr_km[i])
-            ix = potmid(i, 1., 1., inner_approx * sigintr_km[i])
+            ax = potin(i, 1, 1, inner_approx * sigintr_km[i])
+            ix = potmid(i, 1, 1, inner_approx * sigintr_km[i])
             if abs((ix - ax) / ix) > 1.0e-4:
                  txt = f"Failed test 2: {ix} != {ax}"
                  self.logger.error(txt)
                  raise ValueError(txt)
             ax = math.sqrt(math.pi / 2.) * V0[i] / outer_approx
-            ix = potmid(i, outer_approx * sigintr_km[i], 0., 0.)
+            ix = potmid(i, outer_approx * sigintr_km[i], 0, 0)
             if abs((ix - ax) / ix) > 1.0e-4:
                  txt = f"Failed test 3: {ix} != {ax}"
                  self.logger.error(txt)
                  raise ValueError(txt)
             ax = math.sqrt(math.pi / 2.) * V0[i] / outer_approx
-            ix = potmid(i, 1., 1., outer_approx * sigintr_km[i])
+            ix = potmid(i, 1, 1, outer_approx * sigintr_km[i])
             if abs((ix - ax) / ix) > 1.0e-4:
                  txt = f"Failed test 4: {ix} != {ax}"
                  self.logger.error(txt)
@@ -442,7 +442,7 @@ class MGE(data.Data):
 
     def _intrin_spher_grid(self, low, up, P, Q, sigma, r0, r1, rho0):
         ''' calculate multi-dimensional integrals'''
-        epsabs = 1.49e-8 # 1.0e-8
+        epsabs = 1.49e-8
         epsrel = 1.49e-8
         limit = 150
         # check integration limits
@@ -479,14 +479,14 @@ class MGE(data.Data):
         sph = math.sin(y)
         cph = math.cos(y)
         c = math.sqrt(2.) * sigma / \
-            math.sqrt((cph**2.+(sph/P)**2.)*sth*sth + (cth/Q)**2.)
+            math.sqrt((cph**2+(sph/P)**2)*sth*sth + (cth/Q)**2)
         term1 = r0*math.exp(-(r0/c)**2) - \
                 0.5*c*math.sqrt(math.pi)*special.erf(r0/c)
         term2 = r1*math.exp(-(r1/c)**2) - \
                 0.5*c*math.sqrt(math.pi)*special.erf(r1/c)
         answ = 0.5*rho0*c*c*(term1 - term2)*sth
         # the orbit library folds the 8 symmetries into one octant:
-        return answ * 8.
+        return answ * 8
 
     def _intrin_radii(self, total_mass, pintr, qintr, sigintr_km, dens, dir):
         settings = self.config.settings.orblib_settings
@@ -499,11 +499,11 @@ class MGE(data.Data):
         quad_lr = np.zeros(nr + 1)
         for i in range(nr):
             quad_lr[i] = \
-                10. ** (rlogmin + (rlogmax - rlogmin) * (i - 1.0) / (nr - 1.0))
+                10. ** (rlogmin + (rlogmax - rlogmin) * (i - 1) / (nr - 1))
         quad_lr[0] = 0.0
-        quad_lr[nr] = 10. ** rlogmax * 100.0
-        quad_lth = np.array([0., math.pi / 2])
-        quad_lph = np.array([0., math.pi / 2])
+        quad_lr[nr] = 10 ** rlogmax * 100.0
+        quad_lth = np.array([0, math.pi / 2])
+        quad_lph = np.array([0, math.pi / 2])
 
         low = (quad_lth[0], quad_lph[0])
         up = (quad_lth[1], quad_lph[1])
@@ -554,13 +554,13 @@ class MGE(data.Data):
         quad_lr = np.zeros(quad_nr + 1)
         quad_lr[0] = 0.
         for i in range(1, quad_nr):
-            quad_lr[i] = 10.**(rlogmin +
+            quad_lr[i] = 10**(rlogmin +
                 (rlogmax - rlogmin + math.log10(0.5)) * i / quad_nr)
-        quad_lr[quad_nr] = max(10. ** rlogmax * 100., max(sigobs_km) * 10.)
+        quad_lr[quad_nr] = max(10 ** rlogmax * 100, max(sigobs_km) * 10)
 
         # Define the angular bins
         quad_lth = np.zeros(quad_nth + 1)
-        quad_lth[0] = 0.
+        quad_lth[0] = 0
         for i in range(1, quad_nth):
             quad_lth[i] = math.pi / 2 * i / quad_nth
         quad_lth[quad_nth] = math.pi / 2
@@ -593,13 +593,13 @@ class MGE(data.Data):
         self.logger.debug('Percent of the Mass inside the projected grid: '
                           f'{sum(quad_grid) / total_mass * 100}.')
         quad_grid /= total_mass
+        quad_grid = np.ravel(quad_grid, order='F')
 
         self.logger.debug(f'quad_grid: {quad_grid}')
         mass_qgrid_filename = \
             self.config.settings.io_settings['output_directory'] + \
                 f'{dir}/' + 'mass_qgrid.ecsv'
-        qgrid_table = table.Table([np.ravel(quad_grid, order='F')],
-                                  names=('mass_qgrid',))
+        qgrid_table = table.Table([quad_grid], names=('mass_qgrid',))
         qgrid_table.meta = \
             {'quad_nph': quad_nph, 'quad_nth': quad_nth,'quad_nr': quad_nr}
         qgrid_table.write(mass_qgrid_filename,
